c4c3fed65216e6f66673ddad8a4dea371fb75874
Xessen <seyhmusb@yandex.com>
2021-03-10 00:15:43 +0300

    NN works!!!!!!!

--- models.py
+++ models.py
@@ -5,31 +5,39 @@ x_train,x_test,y_train,y_test=ImagePreprocess(["test0","test1"])
 print(y_test)
 
 class NeuralNetwork:
-    def __init__(self,layer_dims,layer_func,learning_rate,iteration,score=None):
+    def __init__(self,layer_dims,layer_func,learning_rate,iteration,train_score=None,test_score=None):
         self.layer_func=layer_func
         self.layer_dims=layer_dims
         self.learning_rate=learning_rate
         self.iteration=iteration
-        self.score=score
+        self.test_score=test_score
+        self.train_score=train_score
     def train(self,X,Y):
         costs=[]
         parameters=param_init(self.layer_dims)
-        for i in range(0,self.iteration):
+        for i in range(self.iteration+1):
             AL, caches = forward_model(X,parameters,self.layer_func)
             cost = compute_cost(AL,Y)
             grads = backward_model(AL,Y,caches,self.layer_func)
 
             parameters = update_parameters(parameters,grads,self.learning_rate)
-
+            costs.append(cost)
             if i%100 == 0:
                 print (f"Cost after iteration {i}: {cost}")
-                costs.append(cost)
+        self.train_score=np.divide((np.sum(costs)-np.mean(costs)),np.sum(costs))*100
+        print(f"Training Accuracy: {self.train_score}")    
         return parameters
     def predict(self,X,parameters):
         result,cache=forward_model(X,parameters,self.layer_func)
         return result
+    def score(self,X,Y,parameters):
+        result,cache=forward_model(X,parameters,self.layer_func)
+        self.test_score=np.divide(np.sum(Y)-np.mean(np.abs(np.subtract(Y,result))),np.sum(Y))*100
+        print(f"Accuracy:{self.test_score}")
+        
 
-nn=NeuralNetwork([19200,16,8,1],["relu*2","sigmoid*1"],0.005,1000)
+
+nn=NeuralNetwork([19200,16,4,8,1],["relu*3","sigmoid*1"],0.005,1000)
 param=nn.train(x_train,y_train)
 
-print(nn.predict(x_test,param))
+nn.score(x_test,y_test,param)
