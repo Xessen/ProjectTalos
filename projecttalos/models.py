faf386a7d759bd8b46908c6f5f3eedea84689340
Xessen <seyhmusb@yandex.com>
2021-03-16 19:27:53 +0300

    Added LinearRegression and features

--- projecttalos/models.py
+++ projecttalos/models.py
@@ -81,14 +81,76 @@ class NeuralNetwork:
 
 
 class LinearRegression:
+    """
+    :This model calculates coefficients,intercept and score(based on LinearRegression.score()):
+
+    :param coefs: Numpy arrays which contains the coefficients of the model
+    :param intercept: Numpy array which contains the intercept of the model
+    :param score: R^2 value of the model
+
+
+    """
     def __init__(self,coefs=None,score=None,intercept=None):
         self.coefs=coefs
         self.score=score
         self.intercept=intercept
 
     def train(self,X,Y):
+        """
+        :Trains the model by given X and Y values:
+        :This function takes 2 parameters:
+
+        :param X: Input features of numpy arrays X.shape should be (number_of_training_examples,number_of_features)
+        :param Y: Output values of numpy arrays Y.shape should be (number_of_training_examples,output_value) 
+        
+        :return: Returns coefficients and intercept value which can be used in predict and score function
+        
+        """
+        X=np.insert(X,0,np.ones((X.shape[0])),axis=1)
         weights=np.dot(np.linalg.inv(np.dot(X.T,X)),np.dot(X.T,Y))
         self.intercept=weights[0]
         self.coefs=weights[1:]
-        print(weights)
+        return self.coefs,self.intercept
 
+    def predict(self,X,coefs=None,intercept=None):
+        """
+        :Predicts the output by given X and already calculated coefficients and intercept(It can be manually initialized):
+        :This function takes 3 parameters:
+
+        :param X: Input features of numpy arrays X.shape should be (number_of_training_examples,number_of_features)
+        :param coefs: Numpy arrays which contains the coefficients of the model (it is automatically initialized if you trained the model and did not pass any arguments with hand)
+        :param intercept: Numpy array which contains the intercept of the model (it is automatically initialized if you trained the model and did not pass any argurments with hand)
+
+        :return: Returns the predicted value(s)
+
+
+        """
+        if coefs==None and intercept==None:
+            coefs=self.coefs
+            intercept=self.intercept
+        prediction=np.dot(coefs.T,X.T)+intercept
+        return prediction
+    
+    def score(self,X,Y,coefs=None,intercept=None):
+        """
+        
+        :This function takes 4 parameters:
+
+        :param X: Input features of numpy arrays X.shape should be (number_of_training_examples,number_of_features)
+        :param Y: Output values of numpy arrays Y.shape should be (number_of_training_examples,output_value) 
+        :param coefs: Numpy arrays which contains the coefficients of the model (it is automatically initialized if you trained the model and did not pass any arguments with hand)
+        :param intercept: Numpy array which contains the intercept of the model (it is automatically initialized if you trained the model and did not pass any argurments with hand)
+
+
+        :return: Returns R^2 score of the model by given parameters
+
+
+        """
+        if coefs==None and intercept==None:
+            coefs=self.coefs
+            intercept=self.intercept
+        prediction=np.dot(coefs.T,X.T)+intercept
+        self.score=(1-(np.sum((Y-prediction)**2)/np.sum((Y-np.mean(Y))**2)))
+        return self.score
+            
+        
